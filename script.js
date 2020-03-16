716ce34a4cbd9d045f40eaa019648ae2d2d8cd17
Daklin2 <daklin754@gmail.com>
2020-03-16 15:05:58 +0300

    feat: off/on phones

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ nav.addEventListener('click', event => {
   });
   event.target.classList.add('linkActive');
 });
+
+//slider
+
+const phones = document.querySelectorAll(".slider .phone__screen");
+
+phones.forEach( phone => phone.addEventListener( "click", event => {
+  if ( event.target.classList.contains("hidden") ) {
+    event.target.classList.remove("hidden");
+  } else {
+    event.target.classList.add("hidden");
+  }
+}));
