d431c6ee871feecca6beb91a180458230d0c880f
Daklin2 <daklin754@gmail.com>
2020-03-17 04:03:46 +0300

    feat: implement portfolio part of DOM task

--- script.js
+++ script.js
@@ -1,47 +1,21 @@
-const switchScreen = () => {
-  const phones = document.querySelectorAll(".slider .phone__screen");
-  phones.forEach( phone => phone.addEventListener( "click", event => {
-    if ( event.target.classList.contains("hidden") ) {
-      event.target.classList.remove("hidden");
-    } else {
-      event.target.classList.add("hidden");
-    }
-  }));
-}
-
 const switchNav = () => {
   const nav = document.getElementById('nav');
   nav.addEventListener('click', event => {
     nav.querySelectorAll('a').forEach(item => {
-      item.classList.remove('linkActive');
+      item.classList.remove('active_nav');
     });
-    event.target.classList.add('linkActive');
+    event.target.classList.add('active_nav');
   });
 }
 
+
 const switchSlide = () => {
-  let currentSlide = 0;
-  arrow.forEach( arrow => arrow.addEventListener("click", event => {
-    (currentSlide === 1) ? currentSlide-- : currentSlide++;
-
-    sliderBackground.classList.value = "slider";
-    sliderBackground.classList.add(slide_bgcolor[currentSlide]);
-
-    slideContainer.innerHTML = slides[currentSlide];
-
-    switchScreen();
-  }));
-}
-
-const sliderBackground = document.querySelector(".slider");
-const slideContainer = document.querySelector(".phones");
-const arrow = document.querySelectorAll(".slider .arrow");
-const slide_bgcolor = {
-  0: "red",
-  1: "green"
-}
-const slides = {
-  0: [`<div class="phone phone_vertical">
+  const slider_slidesBgcolor = {
+    0: "red",
+    1: "green"
+  }
+  const slider_slidesPattern = {
+    0: [`<div class="phone phone_vertical">
           <div class="phone__screen"></div>
           <div class="phone__case"></div>
           <div class="phone__shadow"></div>
@@ -51,9 +25,70 @@ const slides = {
           <div class="phone__case"></div>
           <div class="phone__shadow"></div>
         </div>`],
-  1: [`<img width="517" height="513" style="padding: 46px 260px;" src="./assets/slider/2-phones.png" alt="Phone Vertical">`]
-};
+    1: [`<img width="517" height="513" style="padding: 46px 260px;" src="./assets/slider/2-phones.png" alt="Phone Vertical">`]
+  };
+
+  let currentSlide = 0;
+  slider_arrows.forEach( arrow => arrow.addEventListener("click", event => {
+    (currentSlide === 1) ? currentSlide-- : currentSlide++;
+    slider_fullSection.classList.value = "slider";
+    slider_fullSection.classList.add(slider_slidesBgcolor[currentSlide]);
+
+    slider_slideContainer.innerHTML = slider_slidesPattern[currentSlide];
+
+    switchScreen();
+  }));
+}
+
+
+const switchScreen = () => {
+  const slider_phones_screen = document.querySelectorAll(".slider .phone__screen");
+  slider_phones_screen.forEach( phone => phone.addEventListener( "click", event => {
+    if ( event.target.classList.contains("hidden") ) {
+      event.target.classList.remove("hidden");
+    } else {
+      event.target.classList.add("hidden");
+    }
+  }));
+}
+
+
+const switchTags = () => {
+  portfolio_filterTags.addEventListener('click', event => {
+    if(!event.target.classList.contains("active_filter")) {
+      let portfolioPictures = [...portfolio_containerOfItems.querySelectorAll(".portfolio_picture")];
+      portfolioPictures.push(portfolioPictures.shift());
+      portfolioPictures.forEach( picture => portfolio_containerOfItems.append(picture) );
+    }
+
+    portfolio_filterTags.querySelectorAll("button").forEach(item => item.classList.remove('active_filter'));
+    event.target.classList.add('active_filter');
+  })
+}
+
+
+const tagClick = () => {
+  const portfolio_items = portfolio_containerOfItems.querySelectorAll("li");
+  portfolio_items.forEach(el => el.addEventListener('click', event => {
+    if(event.target.classList.contains('active_item')) event.target.classList.remove('active_item')
+    else {
+      portfolio_items.forEach((el => el.querySelector('img').classList.remove('active_item')));
+      event.target.classList.add('active_item');
+    }
+  }))
+}
+
+
+const slider_fullSection = document.querySelector(".slider");
+const slider_slideContainer = document.querySelector(".phones");
+
+const slider_arrows = document.querySelectorAll(".slider .arrow");
+const portfolio_containerOfItems = document.querySelector('.portfolio__works');
+
+const portfolio_filterTags = document.getElementById('filter');
 
 switchNav();
 switchScreen();
 switchSlide();
+switchTags()
+tagClick()
