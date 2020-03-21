f422efd064ef44188ee668741350b7724218b8f8
Daklin2 <daklin754@gmail.com>
2020-03-21 03:56:32 +0300

    feat: modal function is done

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+class Modal {
+  constructor (modal, form, mess) {
+    this.modal = modal;
+    this.form = form;
+    this.message_container = mess;
+  }
+
+  show () {
+    this.modal.classList.remove('hidden');
+  }
+
+  close () {
+    this.modal.classList.add('hidden');
+  }
+
+  formValue (obj) {
+    let value = this.form.querySelector(obj['node']).value;
+    value = (value === "") ? obj['defaultValue'] : value;
+    return `<p>${obj['fieldName']}: ${value}</p>`;
+  }
+
+  init (message) {
+    this.form.addEventListener('submit', event => {
+      event.preventDefault();
+      let text = '<p>Письмо отправлено</p>' + message['message'].map(obj => this.formValue(obj)).join('');
+      this.message_container.innerHTML = text;
+      this.show();
+    })
+  }
+
+  reset () {
+    this.form.reset();
+  }
+}
+
+
 const navigation = () => {
-  function checkLocation() {
+  function checkLocation () {
     let currentItem = anchors[0];
     anchors.forEach((el) => {
       const elTop = el.getBoundingClientRect().top;
@@ -12,17 +48,18 @@ const navigation = () => {
   const anchors = document.querySelectorAll('section');
   let currentNavigationItem;
 
-  checkLocation();
+  checkLocation ();
   window.addEventListener('scroll', () => {
     currentNavigationItem.classList.remove('active_nav');
     checkLocation();
   });
 }
 
+
 const switchSlide = () => {
   const slider_slidesBgcolor = {
     0: "red",
-    1: "green"
+    1: "blue"
   }
   const slider_slidesPattern = {
     0: [`<div class="phone phone_vertical">
@@ -46,6 +83,7 @@ const switchSlide = () => {
 
     slider_slideContainer.innerHTML = slider_slidesPattern[currentSlide];
 
+    slider_arrows.forEach(arrow => arrow.querySelectorAll('span').forEach(el => el.classList.value = 'arrow_' + slider_slidesBgcolor[currentSlide]));//add color class
     switchScreen();
   }));
 }
@@ -89,6 +127,30 @@ const tagClick = () => {
 }
 
 
+const getQuote_modal = () => {
+  const form_container = document.querySelector("form");
+  const modal_container = document.querySelector(".modal")
+  const modal_message_container = modal_container.querySelector(".modal_message");
+
+  const modal = new Modal(modal_container, form_container, modal_message_container);
+  const modal_message = {
+    'message': [
+      {'node': "input[name='subject']", 'fieldName': "Subject", 'defaultValue': "No subject"},
+      {'node': "textarea[name='description']", 'fieldName': "Description", 'defaultValue': "No description"}
+    ]
+  }
+
+  modal.init(modal_message)
+
+  window.addEventListener('click', event => {
+    if(event.target.classList.contains('modal_background')) {
+      modal.reset();
+      modal.close();
+    }
+  });
+}
+
+
 const slider_fullSection = document.querySelector(".slider");
 const slider_slideContainer = document.querySelector(".phones");
 
@@ -102,6 +164,8 @@ switchScreen();
 switchSlide();
 switchTags();
 tagClick();
+getQuote_modal();
+
 
 
 
